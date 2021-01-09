627418069e4ffd667f314f203a2d277e4f2c8b1d
AbeerAsrani <72503306+AbeerAsrani@users.noreply.github.com>
2021-01-09 18:56:17 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+array_images=[ father.jpg,download.png,teen.jpg,kid.jpg];
+array_names=[Nikhil Asrani,Puneeta Asrani,Daksh Asrani, Abeer Asrani]
+function next(){
+    document.getElementById("next")
+}
